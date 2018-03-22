f144cda459eafc45d15160c0fb830fc0adb606a9
GregDavidson <greg.davidson+git@gmail.com>
2018-03-22 16:56:40 -0700

    consolidated hooks into one function

--- NGender/ngender-rust.el
+++ NGender/ngender-rust.el
@@ -18,33 +18,28 @@
 ; (setq racer-rust-src-path "~/.rust/src/")
 (setq racer-rust-src-path "/usr/local/lib/rustlib/")
 
-;;;; Rust Support
+;;;; Rust Support reference
 ; http://bassam.co/emacs/2015/08/24/rust-with-emacs/
 
-;; Setting up configurations when you load rust-mode
-(add-hook 'rust-mode-hook
-	  '(lambda ()
-	     ;; Enable racer
-	     (racer-activate)
-	     (racer-mode)
-	     
-	     ;; Hook in racer with eldoc to provide documentation
-	     (racer-turn-on-eldoc)
-	     
-	     ;; Use flycheck-rust in rust-mode
-	     ; (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-	     
-	     ;; Use company-racer in rust mode
-	     ;; (set (make-local-variable 'company-backends) '(company-racer))
-	     
-	     ;; Key binding to jump to method definition
-	     (local-set-key (kbd "M-.") #'racer-find-definition)
-	     
-	     ;; Key binding to auto complete and indent
-	     (local-set-key (kbd "TAB") #'racer-complete-or-indent)
-	     ) )
-
 (defun rust-mode-ngender ()
+	;; Enable racer
+	(racer-activate)
+	(racer-mode)
+	
+	;; Hook in racer with eldoc to provide documentation
+	(racer-turn-on-eldoc)
+	
+	;; Use flycheck-rust in rust-mode
+	;; (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+	
+	;; Use company-racer in rust mode
+	;; (set (make-local-variable 'company-backends) '(company-racer))
+	
+	;; Key binding to jump to method definition
+	(local-set-key (kbd "M-.") #'racer-find-definition)
+	
+	;; Key binding to auto complete and indent
+	(local-set-key (kbd "TAB") #'racer-complete-or-indent)
 	(ngender-pitch-mode)
 	(setq orgstruct-heading-prefix-regexp "/[/*] ")
 	(orgstruct-mode)
