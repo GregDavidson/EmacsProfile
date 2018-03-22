b893867b0abf3543bec6e5a9c586209c72699e80
GregDavidson <greg.davidson+git@gmail.com>
2018-03-22 00:03:57 -0700

    new generic version under ~/.emacs.d/NGender

---
+++ NGender/ngender-scala.el
@@ -0,0 +1,12 @@
+;; * Ensime Scala Support -*- lexical-binding: t; -*-
+;; Authors:
+;;	jgd = J. Greg Davidson
+
+;; ** Dependencies - provide and require
+
+;; (require 'ensime)
+
+;; ** The Hook
+
+;; (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
