fb3b5748134065b69e61d8fd854ced6f4d26930e
GregDavidson <greg.davidson+git@gmail.com>
2018-04-11 13:46:37 -0700

    Merged init.el, init-sample.el

--- init.el
+++ init.el
@@ -1,21 +1,29 @@
-;; * User-Me/init-me.el
+;; * .init file in emacs customization directory
 ;; Features requiring significant customization
 ;; are in separate byte-compiled files under
 ;; - NGender/ --> if likely to be useful to others
 ;; - User-Me/ --> otherwise
 
+;; When this is done:
+;; copy it to init-sample.el
+;; check the copy for anything non-generic
+
 ;; Everything after one or more semicolons is a comment for
 ;; humans and is ignored by Emacs.
+
+;; You might want to copy this to init.el so you can
+;; change it without messing up this sample, e.g.
+;;	cp init-sample.el init.el
 
 ;; This file should be loaded by ~/.emacs
 ;; which should be linked to ~/.emacs.d/.emacs
 ;; See README.org file in this directory
 
 ;; Study the README.org file to understand the purpose of
-;; the directory User-Me and the file init-me.el under it.
-;; Change any of these paths if needed.
+;; the directory User-Me and the file User-Me/init-me.el
+;; (You are expected to create both of these!)
+;; Change any of these paths if needed or desired.
 
-(load "ngender")
 ;; Declare any Vendor (3rd-party library) directories here, e.g.
 (ngender-vendor-subdirectory "vendor")
 ;; Declare any (Project) Group directories here, e.g.
@@ -34,6 +42,6 @@
 ;; Remember that Emacs will help you better with init problems if you
 ;; call it with emacs --debug-init
 
-;; You don't want to put much more in this file.  Read the README
-;; files for guidance and communicate with the Emacs Users in the
-;; RPTUG!
+;; You don't want to put much more in this file.  Read the
+;; README files for guidance and communicate with the Emacs
+;; Users in the RPTUG!
