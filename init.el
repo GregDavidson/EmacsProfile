8d20eaf198e4e23291ff5bf35751d40c22ef6464
GregDavidson <greg.davidson+git@gmail.com>
2018-05-11 01:30:41 -0700

    clarified comments and updated code to current ngender api

--- init.el
+++ init.el
@@ -11,33 +11,42 @@
 ;; which should be linked to ~/.emacs.d/.emacs
 ;; See README.org file in this directory
 
+;; This file starts out
+;;	a symbolic link from NGender/init.el to .emacs.d
+;; If you truly need to change this file, then
+;;	(1) remove the link
+;;	(2) copy NGender/init.el to User-Me/init.el
+;;  (3) create a new link to the copy
+;;  (4) edit the copy as needed
+;; NOTE: Avoid changing this file if you can accomplish your
+;; purpose by editing User-Me/init-me.el
+
 ;; Study the README.org file to understand the purpose of
 ;; the directory User-Me and the file init-me.el under it.
 ;; Change any of these paths if needed.
 
-;; Declare any Vendor (3rd-party library) directories here.
+;; Declare any Package (3rd-party library) directories here.
 ;; "vendor" is a common directory for such.
 (when (file-directory-p (expand-file-name "vendor" *ngender-emacs-home*))
-				(ngender-vendor-subdirectory "vendor") )
+				(ngender-emacs-path "vendor") )
 				
-;; Declare any (Project) Group directories here, e.g.
-;; (ngender-group-subdirectory "Group-RPTUG")
+;; Declare any (Project) Group module directories here, e.g.
+;; (ngender-group-dir "Group-RPTUG")
 
 ;; Your personal customization directory path,
 ;; *ngender-user-me*, should be set in your .emacs file
-(ngender-user-subdirectory (file-name-nondirectory *ngender-user-me*))
-;; The Emacs path will be kept ordered as follows, first to last:
-;; (1) Your User Subdirectory
-;; (2) Any Group (Project) Directories
-;; (3) Any Vendor (3rd party extension) directories
-;; (4) Directories for Packages downloaded from Emacs Repositories
+(ngender-user-dir (file-name-nondirectory *ngender-user-me*))
+;; Your module path will be kept ordered as follows, first to last:
+;; (1) Your User Subdirectory (typically named User-Me)
+;; (2) Group (Project) Directories, if any
+;; (3) The *ngender-modules-dir* (typically named NGender)
 
 ;; Now it's time to load your personal customizations:
 (when (file-readable-p (expand-file-name "init-me.el" *ngender-user-me*))
-	(load "init-me") )									; might be compiled
+	(ngender "init-me") )	 ; possibly loads a compiled version
 
 ;; Remember that Emacs will help you better with init problems if you
 ;; call it with emacs --debug-init
 
-;; You don't want to put much more in this file.  Read the README
-;; files for guidance and communicate with the RPTUG Emacs Users.
+;; Read the README files for guidance and communicate with
+;; the RPTUG Emacs Users!
